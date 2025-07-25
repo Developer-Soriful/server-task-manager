8b482030aed3d94dca60e6e971002eb0c5968a03
9389soriful <134063362+9389soriful@users.noreply.github.com>
2025-07-26 01:41:29 +0600

    added

--- index.js
+++ index.js
@@ -71,4 +71,8 @@ app.get('/', (req, res) => {
     res.send('Project is running')
 })
 
+// app.listen(5000, () => {
+//     console.log("Server is running on port 5000");
+// })
+
 export default app;
